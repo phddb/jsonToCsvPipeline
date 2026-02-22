f19b4e481665fc7086891ec958c47051b8045fee
d-H- <davehbenson@gmail.com>
2026-02-22 07:28:53 -0500

    with CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGINS?.split(',') || [],
+    credentials: false,  // if you need cookies/auth headers
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
